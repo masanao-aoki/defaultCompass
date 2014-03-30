bbd5a57678687620bac969ae4cb7f8def0f6e93a
kamem <develo0@gmail.com>
2014-03-30 13:20:27 +0900

    使い勝手が良さそうな.rbの追加 fileList指定フォルダのファイルリストを取得してくれる sqrtルートの計算を返す

--- rb/develo.rb
+++ rb/develo.rb
@@ -5,6 +5,23 @@ module Develo
 		path = fileName.value
 		Sass::Script::String.new(FileTest.exist?(path).to_s)
     end
+
+	def fileList(fileName)
+		array = []
+		dirs = Dir.glob(fileName.value)
+		dirs.each_with_index do |li,i|
+			if i == 0 || li.is_a?(Sass::Script::Color) then
+				array.push(li)
+			else
+				array[i-1] = Sass::Script::String.new("#{array[i-1]}#{li}")
+			end
+		end
+		Sass::Script::List.new(array,',')
+	end
+	
+	def sqrt(x)
+		numeric_transformation(x){|value| Math::sqrt(value)}
+	end
 end
 
 module Sass::Script::Functions
